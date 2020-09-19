24e60321f71d1fc5b4971ed81253239600230dee
Complexor <jozbornn@gmail.com>
2020-09-19 13:23:01 -0700

    nested aeration_sum in playground_2

--- playground/snr_playground_2.py
+++ playground/snr_playground_2.py
@@ -2,16 +2,15 @@ from snr import *
 import random
 
 
-# An interesting new identity for aerated signature convolution
-# Shown in SNR 3.4
-def aeration_sum(d, g, a):
-    out = Seq([0])
-    for k in range(len(g)):
-        out += (d**(k+1)).aerate(a) * x**k * g[k]
-    return out
-
-
 def test_aeration_summation():
+    # An interesting new identity for aerated signature convolution
+    # Shown in SNR 3.4
+    def aeration_sum(d, g, a):
+        out = Seq([0])
+        for k in range(len(g)):
+            out += (d ** (k + 1)).aerate(a) * x ** k * g[k]
+        return out
+
     d = Seq([random.randint(1, 3) for k in range(random.randint(2, 5))])
     g = Seq([random.randint(1, 3) for k in range(random.randint(2, 5))])
     a = random.randint(1, 4)
@@ -161,4 +160,11 @@ def memoization_test():
     print(out)
 
 
-memoization_test()
+# A random signature convolution identity
+def curious_identity_in():
+    d = Seq([1, 1])
+    g = Seq([1, 1])
+    g_f = g.f()
+
+    print(Sig(d) * Sig(g_f))
+    print(d * Seq(Sig(d) * Sig(g)).f())
